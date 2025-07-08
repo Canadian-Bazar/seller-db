263febd397d34e71bf5b7d32e0ca6f43c9293016
Ubuntu <ubuntu@ip-172-31-28-35.ec2.internal>
2025-07-08 15:05:20 +0000

    fix

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ init().then((dbStatus) => {
       allowedHeaders: 'Content-Type',
       credentials: true,
       methods: 'POST, GET, PATCH, PUT, DELETE, HEAD, OPTIONS',
-      origin: [process.env.FRONTEND_URL , 'http://localhost:5174']
+      origin: [process.env.FRONTEND_URL , 'http://localhost:5174' , 'https://seller-canadian-bazar.duckdns.org']
     }),
   )
 
